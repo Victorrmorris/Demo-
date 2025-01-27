5ed4a36eb6549b29a74238e777ee57b1bcccdbfb
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2025-01-26 22:11:49 -0600

    Update new_app_for_testing_FEB.py

--- new_app_for_testing_FEB.py
+++ new_app_for_testing_FEB.py
@@ -36,13 +36,13 @@ if st.sidebar.button("Get Insights"):
         st.sidebar.markdown("""
         **Chatbot Suggestion:**  
         Based on your question, here is a short assessment:  
-        - Housing, including rent, utilities, and internet, formed a significant portion of their spending
-        - Complemented by groceries from both the commissary and local supermarkets
-        - Dining out included casual meals at local bakeries and restaurants, with occasional splurges on fine dining for special occasions
-        - Transportation costs covered fuel, public transit, and car maintenance
-        -Recreation and travel featured prominently, with visits to local attractions, weekend trips to neighboring countries, and concerts
-        - Other expenses included childcare, school supplies, shopping for seasonal needs, healthcare, and occasional donations or gifts
-        -Seasonal spikes in travel and heating expenses during the holidays and winter months are notable, alongside steady savings contributions to retirement accounts.   
+        - Housing, including rent, utilities, and internet, formed a significant portion of your spending
+        - Complemented by groceries from both the commissary and German markets like Rewe and Edeka
+        - Dining out included frequent meals at the Ratskeller and KFC, with occasional splurges on fine dining for special occasions in Nuremberg
+        - Transportation costs covered fuel, Deutsche Bahn, and BMW maintenance
+        - Recreation and travel featured prominently, with visits to local Bavarian attractions, weekend trips to Austria, and Rock Im Park concerts
+        - Other expenses included childcare, school supplies, shopping for seasonal needs, dental, and occasional donations or gifts
+        -Seasonal spikes in travel and heating expenses during the holidays and winter months are notable, alongside steady savings contributions to your AMEX account.   
         """)
     else:
         st.sidebar.warning("Please type a question to get insights.")
